8654148518eadc1b19eec9b54be2a555b83e0a5b
Alan Pereyro <alanpereyro0@gmail.com>
2023-04-01 19:59:54 -0400

    created another html and css and made 4 questions

--- javascript.js
+++ javascript.js
@@ -1,4 +1,56 @@
-// We need to track which question we are currently on
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const timerText = document.querySelector('#timer');
+
+let currentQuestion= {}
+let acceptingAnswers = true
+let timer = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        choice1: '<js>',
+        choice2: '<script>',
+        choice3: '<scripting>',
+        choice4: '<javascript>',
+        answer: 2,
+    },
+    {
+        question: 'How do you write Hello World in an alert box?',
+        choice1: 'alert("hello World")',
+        choice2: 'msg("helloo World")',
+        choice3: 'msgbox("helloo World")',
+        choice4: 'alertbox("helloo World")',
+        answer: 1,
+    },
+    {
+        question: 'Which operator is used to assign a value to a variable?',
+        choice1: '*',
+        choice2: '=',
+        choice3: '-',
+        choice4: 'x',
+        answer: 2
+    },
+    {
+        question: 'How to write an IF statement in JavaScript?',
+        choice1: 'if i--5 then',
+        choice2: 'if i = 5',
+        choice3: 'if i = 5 then',
+        choice4: 'if (i==5)',
+        answer: 4,
+            
+    }
+]
+
+const maxQuestions = 4
+
+
+
+
+
+
 var QIndex = 0;
 var time = 70;
 var userScores = "";
